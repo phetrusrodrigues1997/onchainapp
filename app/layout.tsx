a7f82d0a493c2a8ad6f5a89a8bd7edecea4aeed6
Phetrus <phetrusrodrigues@gmail.com>
2025-03-02 15:12:53 +0000

    Web Analytics via vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Providers } from './providers';
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="bg-background dark">
       <SpeedInsights/>
+      <Analytics/>
         <Providers>{children}</Providers>
       </body>
     </html>
