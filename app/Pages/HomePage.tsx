4f99831d0b894c15aac94691f8993503fd7249d6
Phetrus <phetrusrodrigues@gmail.com>
2025-03-13 19:28:02 +0000

    Homepage looks cool now2

--- app/Pages/HomePage.tsx
+++ app/Pages/HomePage.tsx
@@ -5,7 +5,7 @@ const images = [
   "https://agnautacouture.com/wp-content/uploads/2012/12/jean-shrimpton-in-ny-by-david-bailey-2.jpg",
   "https://plus.unsplash.com/premium_photo-1722018576685-45a415a4ff67?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8c3BhY2V8ZW58MHx8MHx8fDA%3D",
   "https://wallpapershome.com/images/pages/ico_h/27177.jpg",
-  "https://i.pinimg.com/736x/b4/60/56/b46056c1414deccf4593144abd85f202.jpg"
+  "https://media.cnn.com/api/v1/images/stellar/prod/140121185821-first-apple-computer.jpg?q=w_3490,h_2205,x_0,y_0,c_fill"
 ];
 
 const FullScreenCarousel = () => {
