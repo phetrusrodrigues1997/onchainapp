dc011157c967de7d474b2d943844336230975042
Phetrus <phetrusrodrigues@gmail.com>
2025-03-14 14:10:08 +0000

    Home page new update

--- app/Pages/HomePage.tsx
+++ app/Pages/HomePage.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { useState, useEffect } from "react";
 
 const images = [
-  "https://images.pexels.com/photos/14751274/pexels-photo-14751274.jpeg?cs=srgb&dl=pexels-anntarazevich-14751274.jpg&fm=jpg",
-  "https://plus.unsplash.com/premium_photo-1722018576685-45a415a4ff67?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8c3BhY2V8ZW58MHx8MHx8fDA%3D",
+  "https://images.pexels.com/photos/40142/new-york-skyline-manhattan-hudson-40142.jpeg?cs=srgb&dl=pexels-pixabay-40142.jpg&fm=jpg",
+  "https://images.pexels.com/photos/6771120/pexels-photo-6771120.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
   "https://agnautacouture.com/wp-content/uploads/2012/12/jean-shrimpton-in-ny-by-david-bailey-2.jpg"
   
   
@@ -63,35 +63,34 @@ const WelcomePage: React.FC<WelcomePageProps> = ({ activeSection, setActiveSecti
       <FullScreenCarousel />
       
       {/* Content overlay */}
-      <div className="relative z-10 min-h-screen flex flex-col">
-        <div className="container mx-auto px-4 flex flex-col h-full">
-          {/* Logo area */}
-          <div className="mt-12 sm:mt-16 flex justify-center">
-            <span className="text-3xl sm:text-4xl font-bold tracking-tight bg-clip-text text-transparent bg-[#d3c81a]">
-              Golden<span className="text-[#f3f3f3]">Eagle</span><span>🦅</span>
-            </span>
-          </div>
-          
-          {/* Main content area */}
-          <div className="flex-grow flex flex-col justify-center items-center max-w-lg mx-auto text-center mt-64">
-  <h2 className="text-2xl font-extrabold text-white leading-tight">
-    Trade Foreign Currencies <br /> & Crypto—All in One!
-  </h2>
-  <div className="mt-8 sm:mt-10 w-full flex justify-center">
-    <button 
-      className=" px-8 py-4 bg-[#d3c81a] text-white font-semibold rounded-lg shadow-lg hover:bg-[#0000aa] transition-colors"
-      onClick={handleEnterClick}
-    >
-      Launch App
-    </button>
+      <div className="relative z-10 flex flex-col">
+  <div>
+    {/* Logo and Button Area */}
+    <div className="absolute top-4 mr-16">
+      <span className="text-xl sm:text-2xl font-bold tracking-tight bg-clip-text text-transparent bg-[#d3c81a]">
+        Golden<span className="text-[#f3f3f3]">Eagle</span><span>🦅</span>
+      </span>
+    </div>
+    <div className="absolute top-4 right-4">
+      <button 
+        className="px-4 py-2 bg-transparent text-white font-semibold rounded-lg shadow-lg hover:bg-[#d3c81a] transition-colors border-2 border-[#d3c81a]"
+        onClick={handleEnterClick}
+      >
+        Launch App
+      </button>
+    </div>
+    
+    {/* Main content area */}
+    <div className="flex-grow flex flex-col justify-center items-center max-w-lg mx-auto text-center mt-[100%]">
+    <h2 className="text-2xl font-extrabold text-white leading-tight font-sans">
+  Trade Foreign Currencies <br /> & Crypto—All in One!
+</h2>
+    </div>
+
+    {/* Empty space at bottom for balance */}
+    <div className="h-16 sm:h-24"></div>
   </div>
 </div>
-
-          
-          {/* Empty space at bottom for balance */}
-          <div className="h-16 sm:h-24"></div>
-        </div>
-      </div>
     </div>
   );
 };
