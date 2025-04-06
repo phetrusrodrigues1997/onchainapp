81c3a68f58cbfa112d3399cc5291f65cb2bd09b7
Phetrus <phetrusrodrigues@gmail.com>
2025-04-06 18:16:35 +0100

    y coordinate home page

--- app/Pages/HomePage.tsx
+++ app/Pages/HomePage.tsx
@@ -151,7 +151,7 @@ const HomePage = ({ activeSection, setActiveSection }: HomePageProps) => {
   });
 
   return (
-    <div className="text-center mb-64 lg:transform lg:translate-y-12">
+    <div className="text-center mb-70 lg:transform lg:translate-y-16">
       <h2 className="text-5xl font-bold mb-4">${totalUSD.toFixed(2)}</h2>
       <div className="flex space-x-4 mt-6">
         <button
