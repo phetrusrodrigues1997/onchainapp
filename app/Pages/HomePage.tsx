fd4f75d85f7b8c48b5d5781b826754a8148fd69f
Phetrus <phetrusrodrigues@gmail.com>
2025-04-09 14:34:56 +0100

    Home page new functionality added notification settings etc 4

--- app/Pages/HomePage.tsx
+++ app/Pages/HomePage.tsx
@@ -158,7 +158,7 @@ const HomePage: React.FC<HomePageProps> = ({ activeSection, setActiveSection }) 
   
 
   return (
-    <div className="shadow-sm text-center mb-72 lg:transform lg:translate-y-24">
+    <div className="shadow-sm text-center mb-70 lg:transform lg:translate-y-26">
       {/* Header Icons for Notifications and Settings */}
       <div className="flex justify-between items-center transform -translate-y-7 ">
   {/* Left side: The username will go here in place of this button */}
