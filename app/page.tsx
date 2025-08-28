512bc6de38993d9fa405b1cf5c011450b562732e
Phetrus <phetrusrodrigues@gmail.com>
2025-08-28 12:27:05 +0100

    mobile button height increased slightly

--- app/page.tsx
+++ app/page.tsx
@@ -491,7 +491,7 @@ export default function App() {
 
       {/* Mobile Bottom Navigation */}
       <div className="fixed bottom-0 left-0 right-0 md:hidden bg-white border-t border-gray-200 z-40">
-        <div className="flex items-center justify-around py-1">
+        <div className="flex items-center justify-around py-2">
           <button
             onClick={() => {
               setActiveSection('home');
