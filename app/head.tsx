1673ab07d202f7d28e82f04ee8df3688287820fb
Phetrus <phetrusrodrigues@gmail.com>
2025-08-30 19:52:39 +0100

    viewport fix 3

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      {/* iOS Safari / PWA status bar controls */}
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+    </>
+  );
+}
