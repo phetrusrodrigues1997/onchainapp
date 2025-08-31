000c719bb3e8cbac79872686e4b8bac9f1197d3e
Phetrus <phetrusrodrigues@gmail.com>
2025-08-31 13:50:06 +0100

    header moved down a bit on mobiles, header border hidden on other pages 2

--- app/page.tsx
+++ app/page.tsx
@@ -389,7 +389,7 @@ export default function App() {
 >
         <div className="max-w-7xl mx-auto flex flex-col">
           {/* Top row with main header elements */}
-          <div className="flex justify-between items-center translate-y-3 md:translate-y-0">
+          <div className="flex justify-between items-center mt-3 md:mt-0">
             <div className="flex items-center flex-1">
               {/* Hamburger menu - shows on both desktop and mobile at left edge */}
               <div>
