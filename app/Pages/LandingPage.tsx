5d1e6a8ba94a018dc5e79bc24197f2ec63e3f914
Phetrus <phetrusrodrigues@gmail.com>
2025-09-05 14:30:57 +0100

    major cosmetic changes4560

--- app/Pages/LandingPage.tsx
+++ app/Pages/LandingPage.tsx
@@ -244,7 +244,7 @@ const LandingPage = ({ activeSection, setActiveSection, isMobileSearchActive = f
         setIsVisible(true);
         // Notify parent that loading is complete
         onLoadingChange?.(false);
-      }, 3500);
+      }, 4000);
     };
     
     // Notify parent that loading started
